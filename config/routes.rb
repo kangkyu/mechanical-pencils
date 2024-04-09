f3f3a646406231b73cd2a8522938648fb999c300
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-04-09 01:13:00 -0700

    Show item image next to item name

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :items, only: [:new, :create]
   root "items#index"
 
   resource :session
