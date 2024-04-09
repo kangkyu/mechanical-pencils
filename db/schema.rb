dd314e9f8b621e4c66e7d9d2d4d65727f51a8b41
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-04-08 19:00:30 -0700

    Add password_digest and has_secure_password for user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_011227) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_09_015219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_011227) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
