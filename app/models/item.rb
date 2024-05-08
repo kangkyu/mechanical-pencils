c46e9583af8c960fe16bbe2f034d825e1c5f7fb3
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-05-08 16:05:33 -0700

    Owned by 1 user

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   validates :name, presence: true
 
+  has_many :ownerships
+
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
