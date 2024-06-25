9ea6ae3caf4857416ad4c4af8580146dd7c211db
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-06-25 16:50:16 -0700

    Add external link for item_group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_195204) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_234429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_195204) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
   end
 
   create_table "items", force: :cascade do |t|
